509a43b2c32dd3d2a4e2ed63ca1666eacd69b251
guilhermercoutinho <guilhermecoutinho.rc@gmail.com>
2024-06-20 22:21:03 -0300

    Criação da estrutura

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,53 @@
+import {
+  Title,
+  Container,
+  TopBackground,
+  Form,
+  ContainerInputs,
+  Input,
+  Button,
+  InputLabel,
+} from "./styles";
+
 function App() {
   return (
-    <div>
-      <h1>Vite + React JS</h1>
-    </div>
-  )
+    <Container>
+      <TopBackground>
+        <img />
+      </TopBackground>
+
+      <Form>
+        <Title>Cadastro Usuário</Title>
+
+        <ContainerInputs>
+          <div>
+            <div>
+              <InputLabel>
+                Nome<span>*</span>
+              </InputLabel>
+              <Input type="text" placeholder="Nome do usuário" />
+            </div>
+
+            <div>
+              <InputLabel>
+                Idade<span>*</span>
+              </InputLabel>
+              <Input type="number" placeholder="Ideda do usuário" />
+            </div>
+          </div>
+
+          <div>
+            <InputLabel>
+              Email<span>*</span>
+            </InputLabel>
+            <Input type="email" placeholder="Email do usuário" />
+          </div>
+
+        </ContainerInputs>
+        <Button>Cadastrar Usuário</Button>
+      </Form>
+    </Container>
+  );
 }
 
-export default App
+export default App;
